26a7a33b2925949ec5fd5efc9432bec7eca7135f
Gustavo <gustavo_alves2010@yahoo.com.br>
2022-09-26 20:39:27 -0300

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const colorOne: Color = {
+  red: 0,
+  green: 0,
+  blue: 0,
+};
+
+console.log(colorOne);
+
+export default colorOne;
